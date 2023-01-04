66f1dad25a01e25626ce672f964c68c4af2d604f
Şuayip Işık <suayip.isik.16@gmail.com>
2023-01-04 20:51:49 +0300

    vector-icon android settings complate

--- src/Router.js
+++ src/Router.js
@@ -3,7 +3,7 @@ import { SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from './views/screens/HomeScreen';
-// import CarGalleryScreen from './views/screens/CarGalleryScreen';
+import CarGalleryScreen from './views/screens/CarGalleryScreen';
 // import CarlistScreen from './views/screens/CarlistScreen';
 // import CarDetailScreen from './views/screens/CarDetailScreen';
 // import ImageScreen from './views/screens/ImageScreen';
@@ -15,8 +15,8 @@ const Router = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        {/* <Stack.Screen name="CarlistScreen" component={CarlistScreen} />
         <Stack.Screen name="CarGalleryScreen" component={CarGalleryScreen} />
+        {/* <Stack.Screen name="CarlistScreen" component={CarlistScreen} />
         <Stack.Screen name="CarDetailScreen" component={CarDetailScreen} />
         <Stack.Screen name="ImageScreen" component={ImageScreen} /> */}
       </Stack.Navigator>
